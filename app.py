1cab05a6f3c075955bf31c5dffdde8719c7d020f
ercancomen <127237293+ercancomen@users.noreply.github.com>
2026-04-15 20:03:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import yt_dlp
 
@@ -6,17 +7,26 @@ app = Flask(__name__)
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('term')
-    ydl_opts = {'format': 'bestaudio', 'noplaylist': True, 'quiet': True}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        # YouTube'da ara ve ilk sonucu al
-        info = ydl.extract_info(f"ytsearch1:{query}", download=False)['entries'][0]
-        return jsonify([{
-            'trackId': info['id'],
-            'trackName': info['title'],
-            'artistName': info['uploader'],
-            'previewUrl': info['url'], # Burası artık 30 sn değil, tam şarkı URL'si olacak
-            'artworkUrl100': info['thumbnail']
-        }])
+    if not query: return jsonify([])
+    
+    ydl_opts = {'format': 'bestaudio/best', 'noplaylist': True, 'quiet': True}
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            # YouTube'da ara ve ilk 10 sonucu al
+            search_results = ydl.extract_info(f"ytsearch10:{query}", download=False)['entries']
+            results = []
+            for info in search_results:
+                results.append({
+                    'trackId': info.get('id'),
+                    'trackName': info.get('title'),
+                    'artistName': info.get('uploader'),
+                    'previewUrl': info.get('url'), # Tam sürüm URL burası!
+                    'artworkUrl100': info.get('thumbnail')
+                })
+            return jsonify(results)
+    except Exception as e:
+        return jsonify([]), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
